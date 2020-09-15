7a34d56e2ed81f9b15298048738fd7c7c1cf9200
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-09-15 15:47:34 -0300

    Implement the config factory `\WPML\PB\Config\Factory`
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-7573

--- tests/phpunit/tests/test-wpml-cornerstone-integration-factory.php
+++ tests/phpunit/tests/test-wpml-cornerstone-integration-factory.php
@@ -21,6 +21,7 @@ class Test_WPML_Cornerstone_Integration_Factory extends OTGS_TestCase {
 		\Mockery::mock( 'overload:WPML_Action_Filter_Loader' )->shouldReceive( 'load' )->with( array(
 			'WPML_PB_Cornerstone_Handle_Custom_Fields_Factory',
 			'WPML_Cornerstone_Media_Hooks_Factory',
+			\WPML\PB\Cornerstone\Config\Factory::class,
 		) );
 
 		$this->assertInstanceOf( 'WPML_Page_Builders_Integration', $subject->create() );
